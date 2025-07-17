30fffc761ae7c49047c286f8cf39ad62791e33f9
MaximusJohnson99 <maximusjohns99@gmail.com>
2025-07-17 17:53:15 +0500

    "Added Home Screen Pause functionality and Sound Controls"

--- app.js
+++ app.js
@@ -12,11 +12,25 @@ const buttonClickSound = new Audio(
   "./Sounds/mixkit-video-game-retro-click-237.wav"
 );
 
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
-}
+const mainSoundTrack = new Audio("./Sounds/game-8-bit-on-278083.mp3");
+mainSoundTrack.loop = true;
+let allSounds = [
+  wallBounceSound,
+  userBlockBounceSound,
+  hitSound,
+  gameOverSound,
+  buttonClickSound,
+  mainSoundTrack,
+];
 
+let startButton = document.getElementById("start-button");
+startButton.addEventListener("click", startGame);
+let resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", resetGame);
+let soundButton = document.getElementById("sound-button");
+soundButton.addEventListener("click", mute);
+let musicButton = document.getElementById("music-button");
+musicButton.addEventListener("click", playBackgroundMusic);
 let lives = 3;
 let livesEmoji = "";
 let score = 0;
@@ -80,6 +94,12 @@ const winScreen = `<div class="game-screen"><h1>YOU WIN!</h1><button onclick="lo
 const completeScreen = `<div class="game-screen"><h1>Congratulations! You have completed the game!</h1></div>`;
 const homeScreen = `<div class="game-screen"><h1>Breakout Blast!</h1><h2>By MaximusJohnson99</h2><button onclick="loadNextLevel()">Play</button></div>`;
 gameDisplay.innerHTML = homeScreen;
+const pauseScreen = document.createElement("div");
+pauseScreen.setAttribute("id", "pause-screen");
+pauseScreen.innerHTML = `<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg fill="#000000" width="800px" height="800px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
+    <path d="M206 1920h548.571V0H206v1920ZM1714.571 0v1920H1166V0h548.571ZM617.43 137.143v1645.714H343.143V137.143h274.286Zm685.714 1645.714h274.286V137.143h-274.286v1645.714Z" fill-rule="evenodd"/>
+</svg>`;
 let playButtons = document.querySelectorAll(".play-buttons");
 let playButtonLeft = playButtons[0];
 let playButtonRight = playButtons[1];
@@ -126,11 +146,10 @@ class Block {
     this.lastHitTime = 0;
     this.coolDownDuration = 900;
     this.currentTime;
-    this.sound = new Audio(
-      "./Sounds/mixkit-player-jumping-in-a-video-game-2043.wav"
-    );
+    this.sound = new Audio(hitSound.src);
   }
   hitBlock() {
+    this.sound.volume = hitSound.volume;
     playSound(this.sound);
     this.currentTime = Date.now();
     if (this.currentTime - this.lastHitTime >= this.coolDownDuration) {
@@ -142,13 +161,13 @@ class Block {
       this.colorNum++;
       this.element.style.backgroundColor = colorArray[this.colorNum];
       if (ballMidX <= this.topLeft[0]) {
-        ballVelocityX = -2;
+        ballVelocityX = -3;
       } else if (ballMidX >= this.bottomRight[0]) {
-        ballVelocityX = 2;
+        ballVelocityX = 3;
       } else if (ballMidY <= this.topLeft[1]) {
-        ballVelocityY = -2;
+        ballVelocityY = -3;
       } else if (ballMidY >= this.bottomRight[1]) {
-        ballVelocityY = 2;
+        ballVelocityY = 3;
       } else {
         ballVelocityX *= -1;
         ballVelocityY *= -1;
@@ -198,16 +217,16 @@ function loadMap() {
 
 function loadPlayer() {
   ballX = gameDisplayWidth / 2 - 10;
-  ballY = gameDisplayHeight - (1 * 55 + 21);
+  ballY = gameDisplayHeight - (1 * 55 + 22);
   userBlockX = 6 * 55;
   randomizer = Math.random();
   if (randomizer < 0.5) {
-    ballVelocityX = 2;
+    ballVelocityX = 3;
   } else {
-    ballVelocityX = -2;
+    ballVelocityX = -3;
   }
-  ballVelocityY = -2;
-  userVelocity = 15;
+  ballVelocityY = -3;
+  userVelocity = 20;
   ball.style.left = ballX + "px";
   ball.style.top = ballY + "px";
   userBlock.style.left = userBlockX + "px";
@@ -222,6 +241,7 @@ async function loadGame() {
     gameDisplay.innerHTML = "";
     loadPlayer();
     loadMap();
+    resetStartButton();
     controls.style.display = "flex";
   } else {
     gameDisplay.innerHTML = completeScreen;
@@ -294,6 +314,7 @@ function stopMoveRight() {
 }
 
 function resetGame() {
+  resetStartButton();
   playSound(buttonClickSound);
   score = 0;
   scoreDisplay.innerText = `Score: ${score}`;
@@ -304,20 +325,21 @@ function resetGame() {
   loadGame();
 }
 
+function resetStartButton() {
+  startButton.innerText = "Start";
+  startButton.removeEventListener("click", pauseGame);
+  startButton.removeEventListener("click", resetGame);
+  startButton.addEventListener("click", startGame);
+}
 function gameOver() {
   if (lives <= 0) {
-    gameOverScreen = `<div class="game-screen"><h1>Game Over!</h1><h2>Score: ${score}</h2><button onclick='resetGame()'>Retry</button></div>`;
+    gameOverScreen = `<div class="game-screen"><h1>Game Over!</h1><h2>Score: ${score}</h2><button onclick="resetGame()">Retry</button></div>`;
     gameDisplay.innerHTML = gameOverScreen;
     playButtons.forEach((playButton) => {
       playButton.style.display = "none";
     });
     lives = 0;
     livesDisplay.innerText = `Lives: ${displayLives()}`;
-    ballX = gameDisplayWidth / 2 - 10;
-    ballY = gameDisplayHeight - (1 * 55 + 21);
-    ballVelocityX = 0;
-    ballVelocityY = 0;
-    userBlockX = 6 * 55;
     clearInterval(moveBallConstant);
     clearInterval(checkBallConstant);
     moveBallConstant = undefined;
@@ -328,11 +350,6 @@ function gameOver() {
     stopMoveRight();
   } else {
     livesDisplay.innerText = `Lives: ${displayLives()}`;
-    ballX = gameDisplayWidth / 2 - 10;
-    ballY = gameDisplayHeight - (1 * 55 + 21);
-    ballVelocityX = 0;
-    ballVelocityY = 0;
-    userBlockX = 6 * 55;
     clearInterval(moveBallConstant);
     clearInterval(checkBallConstant);
     moveBallConstant = undefined;
@@ -346,6 +363,7 @@ function gameOver() {
   playButtons.forEach((playButton) => {
     playButton.style.display = "none";
   });
+  resetStartButton();
 }
 
 function checkCollision() {
@@ -356,7 +374,7 @@ function checkCollision() {
   // check for collision from top
   if (ballY <= 0) {
     playSound(wallBounceSound);
-    ballVelocityY = 2;
+    ballVelocityY = 3;
   }
   // check for collision from bottom, if yes then game over
   if (ballY + 21 >= gameDisplayHeight) {
@@ -367,12 +385,12 @@ function checkCollision() {
   // check for collision from left
   if (ballX <= 0) {
     playSound(wallBounceSound);
-    ballVelocityX = 2;
+    ballVelocityX = 3;
   }
   // check for collision from right
   if (ballX + 21 >= gameDisplayWidth) {
     playSound(wallBounceSound);
-    ballVelocityX = -2;
+    ballVelocityX = -3;
   }
 
   // check for collision with userblock
@@ -384,16 +402,16 @@ function checkCollision() {
   ) {
     if (ballMidX <= userBlockX) {
       playSound(userBlockBounceSound);
-      ballVelocityX = -2;
+      ballVelocityX = -3;
     } else if (ballMidX >= userBlockX + 165) {
       playSound(userBlockBounceSound);
-      ballVelocityX = 2;
+      ballVelocityX = 3;
     } else if (ballMidY <= userBlockY) {
       playSound(userBlockBounceSound);
-      ballVelocityY = -2;
+      ballVelocityY = -3;
     } else if (ballMidY >= userBlockY + 10) {
       playSound(userBlockBounceSound);
-      ballVelocityY = 2;
+      ballVelocityY = 3;
     }
   }
   for (let i = blockList.length - 1; i >= 0; i--) {
@@ -431,6 +449,9 @@ function moveBall() {
 }
 
 function startGame() {
+  startButton.innerText = "Pause";
+  startButton.removeEventListener("click", startGame);
+  startButton.addEventListener("click", pauseGame);
   playSound(buttonClickSound);
   document.addEventListener("keydown", movePlayer);
   document.addEventListener("keyup", stopMovePlayer);
@@ -443,12 +464,12 @@ function startGame() {
     } else {
       moveBallConstant = setInterval(moveBall, 9);
     }
-    ballVelocityY = -2;
+    ballVelocityY = -3;
     randomizer = Math.random();
     if (randomizer < 0.5) {
-      ballVelocityX = 2;
+      ballVelocityX = 3;
     } else {
-      ballVelocityX = -2;
+      ballVelocityX = -3;
     }
   }
   if (checkBallConstant === undefined) {
@@ -460,11 +481,12 @@ function startGame() {
   if (blockList.length <= halfPoint) {
     userVelocity = 25;
   } else {
-    userVelocity = 15;
+    userVelocity = 20;
   }
 }
 
 function winGame() {
+  resetStartButton();
   gameDisplay.innerHTML = winScreen;
   playButtons.forEach((playButton) => {
     playButton.style.display = "none";
@@ -490,3 +512,110 @@ function loadNextLevel() {
   gameDisplay.innerHTML = "";
   loadGame();
 }
+
+function pauseGame() {
+  playSound(buttonClickSound);
+  startButton.innerText = "Resume";
+  startButton.removeEventListener("click", pauseGame);
+  startButton.addEventListener("click", resumeGame);
+  gameDisplay.appendChild(pauseScreen);
+  clearInterval(moveBallConstant);
+  clearInterval(checkBallConstant);
+  moveBallConstant = undefined;
+  checkBallConstant = undefined;
+  playButtons.forEach((playButton) => {
+    playButton.style.display = "none";
+  });
+  document.removeEventListener("keydown", movePlayer);
+  document.removeEventListener("keyup", stopMovePlayer);
+  stopMoveLeft();
+  stopMoveRight();
+}
+
+function resumeGame() {
+  playSound(buttonClickSound);
+  startButton.innerText = "Pause";
+  startButton.removeEventListener("click", resumeGame);
+  startButton.addEventListener("click", pauseGame);
+  if (moveBallConstant === undefined) {
+    if (blockList.length <= halfPoint) {
+      moveBallConstant = setInterval(moveBall, 6);
+    } else {
+      moveBallConstant = setInterval(moveBall, 9);
+    }
+  }
+  if (checkBallConstant === undefined) {
+    if (blockList.length <= halfPoint) {
+      checkBallConstant = setInterval(checkCollision, 2);
+    }
+    checkBallConstant = setInterval(checkCollision, 3);
+  }
+  if (blockList.length <= halfPoint) {
+    userVelocity = 25;
+  } else {
+    userVelocity = 20;
+  }
+  gameDisplay.removeChild(pauseScreen);
+  playButtons.forEach((playButton) => {
+    playButton.style.display = "block";
+  });
+  document.addEventListener("keydown", movePlayer);
+  document.addEventListener("keyup", stopMovePlayer);
+}
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function playBackgroundMusic() {
+  playSound(buttonClickSound);
+  mainSoundTrack.play();
+  let musicSvg = document.getElementById("music-svg");
+  let cancelSvg = musicButton.getElementsByTagName("svg")[0];
+  musicSvg.style.width = "40px";
+  musicSvg.style.height = "40px";
+  cancelSvg.style.display = "none";
+  musicButton.removeEventListener("click", playBackgroundMusic);
+  musicButton.addEventListener("click", stopBackgroundMusic);
+}
+
+function stopBackgroundMusic() {
+  playSound(buttonClickSound);
+  mainSoundTrack.pause();
+  let cancelSvg = musicButton.getElementsByTagName("svg")[0];
+  let musicSvg = document.getElementById("music-svg");
+  musicSvg.style.width = "30px";
+  musicSvg.style.height = "30px";
+  cancelSvg.style.display = "block";
+  musicButton.removeEventListener("click", stopBackgroundMusic);
+  musicButton.addEventListener("click", playBackgroundMusic);
+}
+
+function mute() {
+  let cancelSvg = soundButton.getElementsByTagName("svg")[0];
+  let soundSvg = document.getElementById("sound-svg");
+  soundSvg.style.width = "30px";
+  soundSvg.style.height = "30px";
+  cancelSvg.style.display = "block";
+  soundButton.removeEventListener("click", mute);
+  soundButton.addEventListener("click", unmute);
+  allSounds.forEach((sound) => {
+    sound.volume = 0;
+  });
+}
+
+function unmute() {
+  playSound(buttonClickSound);
+  let cancelSvg = soundButton.getElementsByTagName("svg")[0];
+  let soundSvg = document.getElementById("sound-svg");
+  soundSvg.style.width = "40px";
+  soundSvg.style.height = "40px";
+  cancelSvg.style.display = "none";
+  soundButton.removeEventListener("click", unmute);
+  soundButton.addEventListener("click", mute);
+  allSounds.forEach((sound) => {
+    sound.volume = 1.0;
+  });
+}
+
